d41797bd3f8eab48b97543e96e418b1101f6fc67
mariana regueira <regueiram7@gmail.com>
2021-10-09 17:54:14 -0500

    Created Models, Controllers and List View

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movie_num = 0
+
+Movie.destroy_all
+10.times do
+  url = "https://api.themoviedb.org/3/movie/top_rated?api_key=68f2be6f58f038faa125bc9358b5b958"
+  movie_serialized = URI.open(url).read
+  movie = JSON.parse(movie_serialized)
+  Movie.create(title: movie['results'][movie_num]['title'], overview: movie['results'][movie_num]['overview'], poster_url: movie['results'][movie_num]['poster_path'], rating: movie['results'][movie_num]['vote_average'])
+  movie_num += 1
+end

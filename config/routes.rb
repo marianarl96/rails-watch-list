9f01f2e8fa9f57a645ca5f88fe83a40e7ce5f7da
mariana regueira <regueiram7@gmail.com>
2021-10-13 21:03:13 -0500

    Router, Controllers and Views are done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists
+  resources :lists do
+    resoures :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
